2a241919d41a285344db6f6aef800e5b28e4dca6
martreba <martreba@greeny.cs.tlu.ee>
2016-09-28 15:19:43 +0300

    login funktsioon valmis

--- functions.php
+++ functions.php
@@ -1,45 +1,53 @@
-<?php
-
+<?php 
+	
+	$database = "if16_romil";
 	// functions.php
-	require("../../../config.php")
-	var_dump($GLOBALS);
-	require("functions.php")
 	
-	function hello ($eesnimi, $perenimi)	{
-		return "Tere tulemast $eesnimi $perenimi!";
-		
-	
-	}
-	echo ("Martin", "Rebane")
-	
-	function signup ($email, $password)	{
-		$database = "if16_romil";
-		
+	function signup($email, $password) {
 		
 		$mysqli = new mysqli($GLOBALS["serverHost"], $GLOBALS["serverUsername"], $GLOBALS["serverPassword"], $GLOBALS["database"]);
 		
 		$stmt = $mysqli->prepare("INSERT INTO user_sample (email, password) VALUE (?, ?)");
-		
 		echo $mysqli->error;
-		
-		//asendan küsimärgid
-		//iga märgi kohta tuleb lisada üks täht - mis tüüpi muutuja on
-		// s - string
-		// i - int
-		// d - double
+
 		$stmt->bind_param("ss", $email, $password);
-		
-		//täida käsku
+
 		if ( $stmt->execute() ) {
 			echo "õnnestus";
 		} else {
 			echo "ERROR ".$stmt->error;
 		}
 		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	/*function sum($x, $y) {
+		
+		return $x + $y;
 		
 	}
+	
+	echo sum(12312312,12312355553);
+	echo "<br>";
+	
+	
+	function hello($firstname, $lastname) {
+		return 
+		"Tere tulemast "
+		.$firstname
+		." "
+		.$lastname
+		."!";
 	}
 	
-	
-	
+	echo hello("romil", "robtsenkov");
+	*/
+
 ?>
